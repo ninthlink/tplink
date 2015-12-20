ef95e22459a75457ebdd8cb5b0843255f240b761
alex chousmith <alex@ninthlink.com>
2015-12-20 14:01:39 -0800

    add comments and wrap in (function(angular) { ... })(window.angular);

--- includes/app.js
+++ includes/app.js
@@ -1,179 +1,205 @@
-var tpApp = angular.module('tpApp', ['angular-svg-round-progress']);
-
-tpApp.controller('tplinkController', function ( $scope, $filter, $timeout ) {
-  // __ seconds countdown til video auto plays
-  $scope.video_every = 60;
-  $scope.video_autocountdown = true;
-  // round the time calculations to __ decimal places
-  $scope.time_decimals = 2;
-  // "estimated" mbps
-  $scope.mbps = {
-    ac: 1300,
-    ad: 2800,
-    eth: 1000
-  };
-  // array of different types of file items to compare
-  $scope.files = [
-    {
-      type: 'pdf',
-      name: 'PDF',
-      qty: 1000,
-      mb: 0.6
-    },
-    {
-      type: 'ebook',
-      name: 'Ebooks',
-      qty: 1000,
-      mb: 1
-    },
-    {
-      type: 'photo',
-      name: 'Photos',
-      qty: 1000,
-      mb: 1.5
-    },
-    {
-      type: 'song',
-      name: 'Songs',
-      qty: 1000,
-      mb: 3.5
-    },
-    {
-      type: 'hdmovie',
-      name: 'HD Movie',
-      qty: 1,
-      mb: 8000
-    },
-    {
-      type: 'fourk',
-      name: '4K Movie',
-      qty: 1,
-      mb: 100000
-    }
-  ];
+(function (angular) {
   
-  $scope.resetFileTimes = function() {
-    // loop through for each
-    angular.forEach( $scope.files, function( value, key ) {
-      if ( !value.hasOwnProperty('fsize') ) {
-        // calculate total fsize for each "transfer"
-        if ( value.qty > 1 ) {
-          value.fsize = value.qty * value.mb;
-        } else {
-          value.fsize = value.mb;
-        }
-      }
-      if ( !value.hasOwnProperty('mbstr') ) {
-        // add a "pretty" mb / gb size
-        if ( value.mb < 1000 ) {
-          value.mbstr = value.mb +'MB';
-        } else {
-          value.mbstr = ( value.mb / 1000 ) +'GB';
-        }
-      }
-      // and (re)init timing..
-      value.timing = {
-        ac: {
-          dur: 0,
-          time: 0
-        },
-        ad: {
-          dur: 0,
-          time: 0
-        },
-        eth: {
-          dur: 0,
-          time: 0
-        }
-      };
-    });
-  };
-  $scope.resetFileTimes();
+  // set up our angular app referenced in <html ng-app="...", with directives
+  var tpApp = angular.module('tpApp', ['angular-svg-round-progress']);
   
-  $scope.calcFileTimeLoop = function() {
-    // loop through for each
-    angular.forEach( $scope.files, function( value, key ) {
-      var thisrow = value;
-      angular.forEach( $scope.mbps, function( value, key ) {
-        // calcs
-        thisrow.timing[key].time = thisrow.fsize / value;
+  // set up our controller referenced in <body ng-controller="...">
+  tpApp.controller( 'tplinkController', tplinkController );
+  
+  // inject controller dependencies
+  tplinkController.$inject = [ '$scope', '$filter', '$timeout' ];
+  
+  // actual tplinkController function starts here
+  function tplinkController( $scope, $filter, $timeout ) {
+    
+    // __ seconds countdown til video auto plays
+    $scope.video_every = 60;
+    $scope.video_autocountdown = true;
+    
+    // round the time calculations to __ decimal places
+    $scope.time_decimals = 2;
+    
+    // "estimated" mbps
+    $scope.mbps = {
+      ac: 1300,
+      ad: 2800,
+      eth: 1000
+    };
+    
+    // array of different types of file items to compare
+    $scope.files = [
+      {
+        type: 'pdf',
+        name: 'PDF',
+        qty: 1000,
+        mb: 0.6
+      },
+      {
+        type: 'ebook',
+        name: 'Ebooks',
+        qty: 1000,
+        mb: 1
+      },
+      {
+        type: 'photo',
+        name: 'Photos',
+        qty: 1000,
+        mb: 1.5
+      },
+      {
+        type: 'song',
+        name: 'Songs',
+        qty: 1000,
+        mb: 3.5
+      },
+      {
+        type: 'hdmovie',
+        name: 'HD Movie',
+        qty: 1,
+        mb: 8000
+      },
+      {
+        type: 'fourk',
+        name: '4K Movie',
+        qty: 1,
+        mb: 100000
+      }
+    ];
+    
+    $scope.resetFileTimes = function() {
+      // loop through for each
+      angular.forEach( $scope.files, function( value, key ) {
+        if ( !value.hasOwnProperty('fsize') ) {
+          // calculate total fsize for each "transfer"
+          if ( value.qty > 1 ) {
+            value.fsize = value.qty * value.mb;
+          } else {
+            value.fsize = value.mb;
+          }
+        }
+        if ( !value.hasOwnProperty('mbstr') ) {
+          // add a "pretty" mb / gb size
+          if ( value.mb < 1000 ) {
+            value.mbstr = value.mb +'MB';
+          } else {
+            value.mbstr = ( value.mb / 1000 ) +'GB';
+          }
+        }
+        // (re)init "timing" forEach..
+        value.timing = {
+          ac: {
+            dur: 0,
+            time: 0
+          },
+          ad: {
+            dur: 0,
+            time: 0
+          },
+          eth: {
+            dur: 0,
+            time: 0
+          }
+        };
       });
-    });
-  };
-  $scope.calcFileTimeLoop();
-  
-  // watch for changes in the 11ad mbps
-  $scope.$watch('mbps.ad', function( newvalue, oldvalue, label ) {
-    if ( newvalue != oldvalue ) {
-      $scope.calcFileTimeLoop();
-    }
-  });
-  
-  // format time __ seconds or __ minutes __ seconds : maybe make a directive?
-  $scope.formatTime = function ( t ) {
-    var oot = $filter('number')(t, 2);
-    if ( oot > 60 ) {
-      var m = Math.floor( oot / 60 );
-      var s = oot % 60;
-      oot = m +' minute'+ ( m > 1 ? 's' : '' ) +' '+ s;
-    }
-    if ( oot == 0 ) {
-      return '...';
-    }
-    return oot +' seconds';
-  };
-  $scope.showvideo = false;
-  // set up timing for video auto play
-  $scope.resetVideo = function() {
-    $scope.video_timeuntil = -1;
-    $scope.video_ticking = false;
-    $scope.showvideo = false;
-  };
-  $scope.resetVideo();
-  $scope.vtimer;
-  
-  $scope.vidTimerTick = function() {
-    $scope.video_ticking = true;
-    $timeout.cancel( $scope.vtimer );
-    $scope.vtimer = $timeout( function() {
-      $scope.video_timeuntil++;
-      if ( $scope.video_timeuntil <= $scope.video_every ) {
-        $scope.vidTimerTick();
-      } else {
-        //console.log($scope.video_every +' SECONDS : PLAY VIDEO!');
-        $scope.showvideo = true;
-        $timeout ( function() {
-          var tpvid = document.getElementById('tpv');
-          // play it now
-          tpvid.play();
-          // and vanilla js attach to "ended" to fire when the html5 video ends
-          tpvid.addEventListener('ended',$scope.vidend,false);
-        }, 100 );
+    };
+    // initial reset set
+    $scope.resetFileTimes();
+    
+    // function to iterate each $scope.files & calc times forEach mbps
+    $scope.calcFileTimeLoop = function() {
+      // loop through for each
+      angular.forEach( $scope.files, function( value, key ) {
+        var thisrow = value;
+        angular.forEach( $scope.mbps, function( value, key ) {
+          // calcs
+          thisrow.timing[key].time = thisrow.fsize / value;
+        });
+      });
+    };
+    // initial calc
+    $scope.calcFileTimeLoop();
+    
+    // watch for changes in the 11ad mbps and re compute accordingly
+    $scope.$watch('mbps.ad', function( newvalue, oldvalue, label ) {
+      if ( newvalue != oldvalue ) {
+        $scope.calcFileTimeLoop();
       }
-    }, 1000 );
-  };
-  
-  $scope.toggleTicking = function() {
-    $timeout.cancel( $scope.vtimer );
-    if ( $scope.video_ticking ) {
-      // then turn off
+    });
+    
+    // format time __ seconds or __ minutes __ seconds : maybe make directive?
+    $scope.formatTime = function ( t ) {
+      var oot = $filter('number')(t, 2);
+      if ( oot > 60 ) {
+        var m = Math.floor( oot / 60 );
+        var s = oot % 60;
+        oot = m +' minute'+ ( m > 1 ? 's' : '' ) +' '+ s;
+      }
+      if ( oot == 0 ) {
+        return '...';
+      }
+      return oot +' seconds';
+    };
+    $scope.showvideo = false;
+    // set up timing for video auto play
+    $scope.resetVideo = function() {
+      $scope.video_timeuntil = -1;
       $scope.video_ticking = false;
-    } else {
-      $scope.vidTimerTick();
-    }
-  };
-  if ( $scope.video_autocountdown ) {
-    // initial start the countdown
-    $scope.toggleTicking();
-  }
-  
-  // handler to trigger when the html5 video has played all the way through
-  $scope.vidend = function(e) {
+      $scope.showvideo = false;
+    };
     $scope.resetVideo();
     
+    $scope.vtimer;
+    
+    $scope.vidTimerTick = function() {
+      $scope.video_ticking = true;
+      $timeout.cancel( $scope.vtimer );
+      // tick every second
+      $scope.vtimer = $timeout( function() {
+        $scope.video_timeuntil++;
+        if ( $scope.video_timeuntil <= $scope.video_every ) {
+          $scope.vidTimerTick();
+        } else {
+          //console.log($scope.video_every +' SECONDS : PLAY VIDEO!');
+          $scope.showvideo = true;
+          $timeout ( function() {
+            var tpvid = document.getElementById('tpv');
+            // play it now
+            tpvid.play();
+            // and vanilla js attach to "ended" to fire when html5 video ends
+            tpvid.addEventListener('ended',$scope.vidend,false);
+          }, 100 );
+        }
+      }, 1000 );
+    };
+    
+    $scope.toggleTicking = function() {
+      $timeout.cancel( $scope.vtimer );
+      if ( $scope.video_ticking ) {
+        // then turn off
+        $scope.video_ticking = false;
+      } else {
+        $scope.vidTimerTick();
+      }
+    };
     if ( $scope.video_autocountdown ) {
       // initial start the countdown
       $scope.toggleTicking();
     }
-  };
-});
+    
+    // handler to trigger when the html5 video has played all the way through
+    $scope.vidend = function(e) {
+      $scope.resetVideo();
+      
+      if ( $scope.video_autocountdown ) {
+        // initial start the countdown
+        $scope.toggleTicking();
+      }
+    };
+    
+    // set $on( $destroy ) to be nice and wipe any $timeout(s)
+    $scope.$on( '$destroy', function( event ) {
+      $timeout.cancel( $scope.vtimer );
+    });
+  }
+  // end of tplinkController
+})(window.angular);
